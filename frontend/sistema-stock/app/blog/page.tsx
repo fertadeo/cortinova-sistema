77a1d4b9d3b59c94d8d59b1a5940124856bba247
Fernando Tadeo <fernandotadeos@gmail.com>
2024-07-06 20:38:09 -0300

    api config

--- frontend/sistema-stock/app/blog/page.tsx
+++ frontend/sistema-stock/app/blog/page.tsx
@@ -1,9 +1,41 @@
-import { title } from "@/components/primitives";
+// pages/users.tsx
+'use client'
+import { useEffect, useState } from 'react';
+import { Users } from '../../types/users'; // Ajusta la ruta según sea necesario
 
-export default function BlogPage() {
+const UsersPage = () => {
+  const [users, setUsers] = useState<Users[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/users')
+        if (!response.ok) {
+          throw new Error('Error fetching users');
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    getUsers();
+  }, []);
+
   return (
     <div>
-      <h1 className={title()}>Blog</h1>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+        
+         {user.id} - {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+export default UsersPage;
