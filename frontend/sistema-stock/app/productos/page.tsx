1b62a60bb0744eb162eaf0e4fe993379447df236
Fernando Tadeo <fernandotadeos@gmail.com>
2024-08-11 00:12:10 -0300

    04-modulo producto: trabajando en TopBar

--- frontend/sistema-stock/app/productos/page.tsx
+++ frontend/sistema-stock/app/productos/page.tsx
@@ -1,38 +1,75 @@
-// pages/users.tsx
 'use client'
-import '@/styles/globals.css'
-import { useEffect, useState } from 'react';
-import { Users } from '../../types/users'; // Ajusta la ruta según sea necesario
+import React, { useRef, useState } from 'react';
+import '@/styles/globals.css';
+import TopBar from '@/components/topBar';
+import { Button, Input, Spinner } from '@nextui-org/react';
+import { AiOutlineUpload } from 'react-icons/ai';
 
-const UsersPage = () => {
-  const [users, setUsers] = useState<Users[]>([]);
+const ProductosPage = () => {
+  const [fileName, setFileName] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    const getUsers = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/users')
-        if (!response.ok) {
-          throw new Error('Error fetching users');
-        }
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
-    };
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click(); // Simula el clic en el input file
+    }
+  };
 
-    getUsers();
-  }, []);
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setFileName(file.name); // Establece el nombre del archivo seleccionado
+      setLoading(true); // Inicia el spinner de carga
+
+      // Simula el procesamiento del archivo durante 3 segundos
+      setTimeout(() => {
+        setLoading(false); // Detiene el spinner después de 3 segundos
+        console.log('Archivo procesado:', file);
+      }, 3000);
+    }
+  };
 
   return (
-    <div>
-      <h1> Lista de Productos</h1>
-      <ul>
-       
-     
-      </ul>
+    <div className='flex justify-center w-full h-full align-middle'>
+      <TopBar>
+        <div className="flex items-center gap-4">
+          <Input
+            type="text"
+            placeholder="Buscar..."
+            className="w-40 px-4 py-2 m-2 border rounded-md"
+          />
+          <div className="relative">
+            <input
+              type="file"
+              accept=".csv"
+              ref={fileInputRef}
+              className="hidden"
+              onChange={handleFileUpload}
+            />
+            <Button
+              onClick={handleButtonClick}
+              className="flex items-center justify-center gap-2 px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+              disabled={loading} // Desactiva el botón mientras se carga
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" color='white' />
+                  Procesando...
+                </>
+              ) : (
+                <>
+                  <AiOutlineUpload className='size-14' />
+                  {fileName ? fileName : 'Cargar desde Excel'}
+                </>
+              )}
+            </Button>
+          </div>
+          <Button className='m-2 bg-secondary-100'>Carga Manual</Button>
+        </div>
+      </TopBar>
     </div>
   );
 };
 
-export default UsersPage;
+export default ProductosPage;
