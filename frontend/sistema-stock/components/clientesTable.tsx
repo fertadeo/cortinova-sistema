0f3fd41604fa66915450f52bfc254add58efbe79
Fernando Tadeo <fernandotadeos@gmail.com>
2024-12-17 08:29:53 -0300

    Style DeleteIcon Hidden

--- frontend/sistema-stock/components/clientesTable.tsx
+++ frontend/sistema-stock/components/clientesTable.tsx
@@ -148,7 +148,7 @@ const ClientesTable: React.FC<Props> = ({ initialUsers }) => {
         );
       case "actions":
         return (
-          <div className="relative flex gap-2">
+          <div className="flex relative gap-2">
             <Tooltip content="Ver">
               <span
                 role="button"
@@ -193,7 +193,7 @@ const ClientesTable: React.FC<Props> = ({ initialUsers }) => {
                   }
                 }}
               >
-                <DeleteIcon />
+                <DeleteIcon className="hidden" />
               </span>
             </Tooltip>
 
@@ -221,7 +221,7 @@ const ClientesTable: React.FC<Props> = ({ initialUsers }) => {
 
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="flex items-center justify-between h-20 p-4 bg-white rounded-lg shadow-medium">
+      <div className="flex justify-between items-center p-4 h-20 bg-white rounded-lg shadow-medium">
         <Input
           isClearable
           placeholder="Buscar"
@@ -234,7 +234,7 @@ const ClientesTable: React.FC<Props> = ({ initialUsers }) => {
         </Button>
       </div>
 
-      <div className="flex-1 mt-4 overflow-auto">
+      <div className="overflow-auto flex-1 mt-4">
         <Table aria-label="Tabla de Clientes" className="w-full">
           <TableHeader columns={filteredColumns}>
             {(column) => (
